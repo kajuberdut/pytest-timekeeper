090edb2847ded750170b35162e96c232baba7df3
Patrick R. Shechet <patrick.shechet@gmail.com>
2019-01-30 22:11:00 -0700

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="pytest-timekeeper",
-    version="0.0.1",
+    version="0.0.2",
     author="Patrick Shechet",
     author_email="patrick.shechet@gmail.com",
     description=("Pytest timeing framework"),
